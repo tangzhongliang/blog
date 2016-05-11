c986a2c5c6b1ba2284c76b5e9ed1987350318d74
tangzhongliang <1071954237@qq.com>
2016-05-11 13:38:53 +0800

    fix

---
+++ _site/create_blog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read name
+time=$(date "+20%y-%m-%d")
+filename="${time}-${name}.html"
+cd _posts
+touch ${filename}
+echo "---" >> ${filename}
+echo "layout: post" >> ${filename}
+echo "title: ${name}" >> ${filename}
+echo "---" >> ${filename}
+vim $filename
+cd ..
