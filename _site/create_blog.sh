c2b4e98233a09559972f53212bd76ee6fa99edba
tangzhongliang <liang@kysto.com>
2016-05-22 17:44:20 +0800

    fix

--- _site/create_blog.sh
+++ _site/create_blog.sh
@@ -8,5 +8,7 @@ echo "---" >> ${filename}
 echo "layout: post" >> ${filename}
 echo "title: ${name}" >> ${filename}
 echo "---" >> ${filename}
+echo "<p>" >> ${filename}
+echo "</p>" >> ${filename}
 vim $filename
 cd ..
